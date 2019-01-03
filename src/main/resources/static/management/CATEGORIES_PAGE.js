1ddd85f1989f3261a626e3de4d16a58135ede423
ravindu wijepala <ravinduw119@gmail.com>
2019-01-03 21:02:58 +0530

    catagoris added

--- src/main/resources/static/management/CATEGORIES_PAGE.js
+++ src/main/resources/static/management/CATEGORIES_PAGE.js
@@ -6,5 +6,5 @@ $(window).load(function () {
 function viewMore(param) {
     localStorage.setItem("selectedCity", "Select City");
     localStorage.setItem("selectedCate", param);
-    window.open("http://localhost:7575/home/listing/vendors", "_self");
+    window.open("home/listing/vendors", "_self");
 }
