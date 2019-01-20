adbe0b245e9930b56be7dc96777c83e2cabdd2e8
ravindu wijepala <ravinduw119@gmail.com>
2019-01-20 18:25:17 +0530

    test page modifications

---
+++ src/main/resources/static/management/client/SEARCH_RESULT.js
@@ -0,0 +1,123 @@
+function getCity() {
+    $.ajax({
+        url: "/advertisement/getAllCities",
+        dataType: 'json',
+        contentType: "application/json",
+        type: 'GET',
+        success: function (data, textStatus, jqXHR) {
+
+            for (var i = 0; i < data.length; i++) {
+
+                $('#cityList').append($('<option>', {
+                    value: data[i],
+                    text: data[i]
+                }));
+            }
+        },
+    });
+}
+
+function getVendorCat() {
+    $.ajax({
+        url: "/advertisement/getAllVendorCat",
+        dataType: 'json',
+        contentType: "application/json",
+        type: 'GET',
+        success: function (data, textStatus, jqXHR) {
+
+            for (var i = 0; i < data.length; i++) {
+
+                $('#cateList').append($('<option>', {
+                    value: data[i],
+                    text: data[i]
+                }));
+            }
+        },
+    });
+}
+
+function loadOn() {
+    $('#allAds\n').empty();
+    search("Select City", "Select Vendor Category");
+};
+
+$('#category').on('click', function () {
+    var city = $("#city").val();
+    var cate = $("#category").val();
+    search(city, cate);
+});
+$('#city').on('change', function () {
+    var city = $("#city").val();
+    var cate = $("#category").val();
+    search(city, cate);
+});
+
+function search(city, cate) {
+
+    console.log(city + "sssssss" + cate + "ddddddddd");
+    $.ajax({
+        url: "/advertisement/getAllads/" + city + "/" + cate,
+        dataType: 'json',
+        contentType: "application/json",
+        type: 'GET',
+        success: function (data, textStatus, jqXHR) {
+
+            $('#allAds\n').empty();
+            if (data.length === 0) {
+                document.getElementById("allAds").innerHTML = "<h1 style='text-align: center'>No Result Found</h1>";
+            } else {
+                for (var i = 0; i < data.length; i++) {
+                    $('#allAds\n').append($("<article class=\"one-third\">\n" +
+                        "                        <figure><a href=\"hotel.html\" title=\"\"><img src='" + data[i].coverImage1 + "' alt=\"\"/></a>\n" +
+                        "                        </figure>\n" +
+                        "                        <div class=\"details\">\n" +
+                        "                            <h3>" + data[i].title + "\n" +
+                        "                                <span class=\"stars\">\n" +
+                        "\t\t\t\t\t\t\t\t\t\t<i class=\"material-icons\">&#xE838;</i>\n" +
+                        "\t\t\t\t\t\t\t\t\t\t<i class=\"material-icons\">&#xE838;</i>\n" +
+                        "\t\t\t\t\t\t\t\t\t\t<i class=\"material-icons\">&#xE838;</i>\n" +
+                        "\t\t\t\t\t\t\t\t\t\t<i class=\"material-icons\">&#xE838;</i>\n" +
+                        "\t\t\t\t\t\t\t\t\t\t<i class=\"material-icons\">&#xE838;</i>\n" +
+                        "\t\t\t\t\t\t\t\t\t</span>\n" +
+                        "                            </h3>\n" +
+                        "                            <span class=\"address\"> " + data[i].city + " •  <a href=\"#\">Show on map</a></span>\n" +
+                        "                            <span class=\"rating\"> 9 /10</span>\n" +
+                        "                            <a  onclick='itemView(" + data[i].id + ")' title=\"Book now\" class=\"gradient-button\">Book now</a>\n" +
+                        "                        </div>\n" +
+                        "                    </article>"));
+
+                }
+                $('#allAds\n').append($(" <div class=\"bottom-nav\">\n" +
+                    "                        <!--back up button-->\n" +
+                    "                        <a href=\"#\" class=\"scroll-to-top\" title=\"Back up\">Back up</a>\n" +
+                    "                        <!--//back up button-->\n" +
+                    "\n" +
+                    "                        <!--pager-->\n" +
+                    "                        <div class=\"pager\">\n" +
+                    "                            <span><a href=\"#\">First page</a></span>\n" +
+                    "                            <span><a href=\"#\">&lt;</a></span>\n" +
+                    "                            <span class=\"current\">1</span>\n" +
+                    "                            <span><a href=\"#\">2</a></span>\n" +
+                    "                            <span><a href=\"#\">3</a></span>\n" +
+                    "                            <span><a href=\"#\">4</a></span>\n" +
+                    "                            <span><a href=\"#\">5</a></span>\n" +
+                    "                            <span><a href=\"#\">6</a></span>\n" +
+                    "                            <span><a href=\"#\">7</a></span>\n" +
+                    "                            <span><a href=\"#\">8</a></span>\n" +
+                    "                            <span><a href=\"#\">&gt;</a></span>\n" +
+                    "                            <span><a href=\"#\">Last page</a></span>\n" +
+                    "                        </div>\n" +
+                    "                        <!--//pager-->\n" +
+                    "                    </div>"));
+
+            }
+        }
+    });
+
+
+}
+
+function itemView(param) {
+    localStorage.setItem("selectedAd", param);
+    window.open("/home/profileview", "_self");
+}
